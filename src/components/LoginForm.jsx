50b0d4bb8d7d6f5e69d23eb69ff4c064b87205fa
Kevin <klzheng@buffalo.edu>
2023-01-20 22:14:23 -0500

    - made all form inputs controlled - added functionality that grabs all occupation and state data from endpoint on page load

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import {
   FormControl,
   FormLabel,
@@ -11,6 +12,38 @@ import {
 
 
 export default function LoginForm() {
+
+  const [userData, setUserData] = useState({
+    name:"", 
+    email:"",
+    password:"",
+    occupation:"",
+    state:"",
+  })
+
+  const [options, setOptions] = useState({})
+
+  const getOptions = (url) => {
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      setOptions({occupations: data.occupations, states: data.states}); console.log({occupations: data.occupations, states: data.states})
+    })
+  }
+  
+  const handleInputChange = (e) => {
+    setUserData({
+      ...userData, 
+      [e.target.name]: e.target.value 
+    })
+  }
+
+  useEffect(() => {
+    const url = "https://frontend-take-home.fetchrewards.com/form"
+    getOptions(url)
+  }, [])
+
+
   return (
     <div className='background'>
       <div className='container'>
@@ -18,34 +51,63 @@ export default function LoginForm() {
 
         <FormControl isRequired>
           <FormLabel>Full Name</FormLabel>
-          <Input type='text' />
+          <Input 
+            name='name' 
+            type='text' 
+            onChange={handleInputChange} 
+            value={userData.name} 
+          />
         </FormControl>
 
         <FormControl isRequired>
           <FormLabel>Email address</FormLabel>
-          <Input type='email' />
+          <Input 
+            type='email' 
+            name='email' 
+            onChange={handleInputChange}
+            value={userData.email}
+          />
         </FormControl>
 
         <FormControl isRequired>
           <FormLabel>Password</FormLabel>
-          <Input type='password' />
+          <Input 
+            type='password' 
+            name='password' 
+            onChange={handleInputChange}
+            value={userData.password}
+          />
         </FormControl>
 
         <FormControl isRequired>
           <FormLabel>Occupation</FormLabel>
-          <Select placeholder='Select occupation'>
-            <option value='option1'>Option 1</option>
-            <option value='option2'>Option 2</option>
-            <option value='option3'>Option 3</option>
+          <Select 
+            placeholder='Select occupation' 
+            name='occupation' 
+            onChange={handleInputChange}
+            value={userData.occupation}
+          >
+            {Object.keys(options).length !== 0 && options.occupations.map((occupation, index) => {
+              return (
+                <option key={index} value={occupation}>{occupation}</option>
+              )
+            })}
           </Select>
         </FormControl>
 
         <FormControl isRequired>
           <FormLabel>State</FormLabel>
-          <Select placeholder='Select state'>
-            <option value='option1'>Option 1</option>
-            <option value='option2'>Option 2</option>
-            <option value='option3'>Option 3</option>
+          <Select 
+            placeholder='Select state'
+            name='state'
+            onChange={handleInputChange}
+            value={userData.state}
+          >
+            {Object.keys(options).length !== 0 && options.states.map((state, index) => {
+              return (
+                <option key={index} value={state.name}>{state.name}</option>
+              )
+            })}
           </Select>
         </FormControl>
 
